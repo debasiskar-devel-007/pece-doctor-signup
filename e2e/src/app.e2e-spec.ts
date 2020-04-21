a7fd0d654d47136ba3846992964c56002edc5594
influxiq1 <himadribar.influxiq@gmail.com>
2020-04-21 13:23:24 +0530

    first commit

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('dna-frontend app is running!');
+    expect(page.getTitleText()).toEqual('pece-doctor-signup app is running!');
   });
 
   afterEach(async () => {
---
+++ src/environments/environment.local.ts
@@ -0,0 +1,5 @@
+export const environment = {
+    production: false,
+    API_URL:'https://iolyy30djb.execute-api.us-east-1.amazonaws.com/dev/api/',
+  };
+  
